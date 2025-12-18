f2e3f5d45ff89e50e7158cec68dd2795f2ca02c5
zikadwialyaputri <zika24ti@mahasiswa.pcr.ac.id>
2025-12-18 17:19:43 +0700

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,7 +5,7 @@
 
         <h1 class="h3 mb-3"><strong>Analytics</strong> Dashboard</h1>
 
-        <div class="row">
+        <div class="row">-
             <div class="col-xl-6 col-xxl-5 d-flex">
                 <div class="w-100">
                     <div class="row">
